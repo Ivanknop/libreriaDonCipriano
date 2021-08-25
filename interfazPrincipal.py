36270a3899890a751c3f0b9fc4e9f3ad5116e214
IvanKnop <ivan.knopoff@gmail.com>
2021-08-25 12:19:07 -0300

    interfaz inicial y tema

---
+++ interfazPrincipal.py
@@ -0,0 +1,30 @@
+import PySimpleGUI as sg
+from tema import tema
+
+import os 
+
+
+def interfazInicial():
+    
+    layout= [
+        [sg.Button('Agregar Libro',font='Italic 20',size=(12,3),key='nuevoLibro'),
+            sg.Button('Editar Libro',font='Italic 20',size=(12,3),key='editLibro'),
+            sg.Button('Borrar Libro',font='Italic 20',size=(12,3),key='delLibro')]
+    ]
+    salir = [[sg.Button('Salir',font='Italic 20',size=(12,3),key='salir')]]
+    return layout + salir
+
+def principal():
+    alto = 500
+    ancho = 900
+    tema()    
+    ventana = sg.Window ('Librería Don Cipriano',interfazInicial(), size = (ancho,alto),element_justification='center')
+    ventana.Finalize()
+
+    while True:
+        evento, value = ventana.read()
+        if (evento == None or evento == 'salir') :
+            break
+        
+    ventana.Close()
+principal()
